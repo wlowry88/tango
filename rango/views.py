19289b02f7f809eeb6ec82a0e2e9ef76f725e8db
Will Lowry <william.c.lowry@gmail.com>
2014-09-28 22:53:43 -0700

    add template for index html, change template path in settings, change index method

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("This is the about page! <a href='/rango/'>Index</a>")
