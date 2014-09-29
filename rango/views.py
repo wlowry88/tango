8b8a46551a379996b15ab078c994e7c65beac988
Will Lowry <william.c.lowry@gmail.com>
2014-09-29 12:38:44 -0700

    set up database query in views file, passed in context dict to template, update template index page

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from rango.models import Category
+
+
 def index(request):
+    #obtains the context from the http request
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    
+    #db query, all categories stored, order by likes desc, top 5
+    #put in the context dict, which is really like instance to view
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
