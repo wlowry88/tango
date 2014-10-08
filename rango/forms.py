1a0649d5a675ada77b1c13cfaa1026d86e1bf72a
Will Lowry <william.c.lowry@gmail.com>
2014-10-07 22:39:28 -0700

    add new classes for user profile and user forms

--- rango/forms.py
+++ rango/forms.py
@@ -29,3 +29,15 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
